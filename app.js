5016b8a977274ee1aa7809599a52f495e20084e7
cubito123 <123569802+cubito123@users.noreply.github.com>
2023-02-06 12:01:57 -0500

    6 de febrero

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: 'back 1'
+      imagenes:'imagenes/back 1.jpg'
+    },
+    {
+      name: 'back 2'
+      imagenes:'imagenes/back 2.jpg'
+    },
+    {
+      name: 'back 3'
+      imagenes:'imagenes/back 3.jpg'
+    },
+    {
+      name: 'back 4'
+      imagenes:'imagenes/back 4.jpg'
+    },
+    {
+      name: 'back 5'
+      imagenes:'imagenes/back 5.jpg'
+    },
+    {
+      name: 'back 6'
+      imagenes:'imagenes/back 6.jpg'
+    },
+  ]
+});
